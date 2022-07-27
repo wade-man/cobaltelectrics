f86d89282c183cc9872d150f447a78e493515d07
Daniel Wade <danny@headred.net>
2022-07-27 17:52:33 +0100

    minfied css, updated readme

--- readme.md
+++ readme.md
@@ -1,1 +1,1 @@
-npx tailwindcss -i ./src/app.css -o ./dist/output.css --watch
+npx tailwindcss -i ./src/app.css -o ./build/cobalt.min.css --minify --watch
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    'index.php'
+    'index.html'
   ],
   theme: {
     fontFamily: {
